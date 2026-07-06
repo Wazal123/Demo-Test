8c3915a9ed31c9958ede24663d3b867321674013
Christian Ivan Espinoza Gaytan <chrits67@gmail.com>
2026-07-06 03:21:44 -0600

    Create funciones.py
    Cree 3 funciones, suma, resta, y area de rectanculo

---
+++ funciones.py
@@ -0,0 +1,21 @@
+def calcular_area_rectangulo(base, altura):
+    """
+    Calcula el área de un rectángulo dados su base y su altura.
+    """
+    area = base * altura
+    return area
+
+def sumar(a, b):
+    """
+    Recibe dos números y devuelve su suma.
+    """
+    resultado = a + b
+    return resultado
+
+
+def restar(a, b):
+    """
+    Recibe dos números y devuelve su resta (a menos b).
+    """
+    resultado = a - b
+    return resultado
